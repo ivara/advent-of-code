c64b04610c0bcc19f53ecc5d8b7aeaca9caa2fb1
Ivar Åsell <ivar.asell@activesolution.se>
2025-12-06 23:54:35 +0100

    day6 part 2 - work in progress

--- 2025/go/day06/solution.go
+++ 2025/go/day06/solution.go
@@ -42,46 +42,78 @@ func part1(input []byte) int {
 	return sum
 }
 
+// w = width (column)
+// h = height (lines)
+func isBlankColumn(lines []byte, column, h int) bool {
+	for i := 0; i < h; i++ {
+		if lines[i][column] != []byte{' '} {
+			return false
+		}
+	}
+	return true
+}
+
+func multiply(numbers []int) {
+	sum := 0
+	for _, n := range numbers {
+		sum *= n
+	}
+	return sum
+}
+
+func add(numbers []int) {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
 func part2(input []byte) int {
 	sum := 0
 	lines := bytes.Split(input, []byte{'\n'})
 
-	// numbers := make([][]int, 5)
-	numbers := make(map[int][]int)
+	width := len(lines[0])
+	height := len(lines)
 
-	// First "invert" the matrix
-	// Then parse fields, last field will be operator!
+	currentNumbers := []int
+	currentOperator := "*"
 
-	// Gather the numbers, loop all lines except the last
-	for i := 0; i < len(lines)-1; i++ {
-		fields := bytes.Fields(lines[i])
+	// Loop over all columns, and for each column
+	// Walk the lines to build numbers
+	for c := 0; c < width; c++ { // Columns
+		// the number we are currently building for this colum
+		var currentNumber []byte
+		for h := 0; h < height; h++ { // Lines
+			if isBlankLine(lines, c, h) {
+				// this means "current block" is done
+				switch currentOperator {
+				case "*":
+					sum += multiply(currentNumbers)
+					break
+				case "+":
+					sum += add(currentNumbers)
+					break
+				}
 
-		for j, f := range fields {
-			number, _ := strconv.Atoi(string(f))
-			numbers[j] = append(numbers[j], number)
+				// clear things up for next block
+				currentOperator := ""
+				currentNumbers = nil
+			} else {
+				// build a number!
+				if operator == "" {
+					// Oh noes! no operator, that means it is in this
+					operator = lines[height-1][c]
+				}
+				number := getNumberFromColumn(lines, c)
+				currentNumbers = append(currentNumbers, number)
+			}
 		}
 	}
 
-	// Figure out the REAL numbers (vertical column wise)
-	for i := 0; i < len(numbers); i++ {
-		// first figure out the longest number in this column, that determines everything }
-	}
-
-	lastLine := lines[len(lines)-1]
-	operations := bytes.Fields(lastLine)
-	for i, o := range operations {
-		toAdd := 0
-		if string(o) == "+" {
-			for _, y := range numbers[i] {
-				toAdd += y
-			}
-		} else if string(o) == "*" {
-			toAdd = 1
-			for _, y := range numbers[i] {
-				toAdd *= y
-			}
-		}
-		sum += toAdd
-	}
 	return sum
+}
+
+func getNumberFromColumn(lines []byte, column c) int {
+	return 0
 }
