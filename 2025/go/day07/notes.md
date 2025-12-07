f5a844fbe76788af4211bd4a4df2f092b19fadce
Ivar Åsell <ivar.asell@activesolution.se>
2025-12-08 00:30:08 +0100

    day07 work in progress

---
+++ 2025/go/day07/notes.md
@@ -0,0 +1,10 @@
+# AoC 2025 day 7
+
+## Part 2
+
+counting number of possible paths the beam can go
+
+count distinct beams?
+
+Problems with test data working extremly fast and getting right answer
+But my real input completely explodes and runs forever (I force quit after a while)
---
+++ 2025/go/day07/solution.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bytes"
+)
+
+// Om du träffar en splitter försvinner du
+// om du träffar en splitter blir det nya beams på -1 och +1
+// Spara en lista på currentBeams
+func part1(input []byte) int {
+	splits := 0
+	lines := bytes.Split(input, []byte{'\n'})
+	beamsMap := make(map[int]bool)
+
+	// Find initial beam (S)
+	for i := 0; i < len(lines[0]); i++ {
+		if lines[0][i] == 'S' {
+			beamsMap[i] = true
+			break
+		}
+	}
+
+	// Start working the field!
+	for i := 1; i < len(lines); i++ {
+		// See if there is a splitter on current paths
+		// bIdx is the value of a column where the beam is currently travelling
+		beamsCopy := getBeamsFromMap(beamsMap)
+		for j := range beamsCopy {
+			if lines[i][beamsCopy[j]] == '^' {
+				splits += 1
+				beamsMap[beamsCopy[j]] = false
+				beamsMap[beamsCopy[j]-1] = true
+				beamsMap[beamsCopy[j]+1] = true
+				// append -1 and +1 to beams
+				// but also remove current beam
+				// Remove element at j, then add the two new beams
+				// beams = append(beams[:j], append(beams[j+1:], tmp[j]-1, tmp[j]+1)...)
+				// beams = removeAtIndex(beams, j)
+				// beams = append(beams, tmp[j]-1, tmp[j]+1)
+
+				// remove beam since it got splitted
+				// beams = beams[:]
+			}
+		}
+	}
+
+	return splits
+}
+
+func getBeamsFromMap(m map[int]bool) []int {
+	count := 0
+	for _, value := range m {
+		if value {
+			count++
+		}
+	}
+
+	var result = make([]int, 0, count)
+	for key, value := range m {
+		if value == true {
+			result = append(result, key)
+		}
+	}
+
+	return result
+}
+
+func getBeamsFromMapV2(m map[int]int) []int {
+	count := 0
+	for _, value := range m {
+		if value > 0 {
+			count++
+		}
+	}
+
+	var result = make([]int, 0, count)
+	for key, value := range m {
+		if value > 0 {
+			result = append(result, key)
+		}
+	}
+
+	return result
+}
+
+func part2(input []byte) int {
+	lines := bytes.Split(input, []byte{'\n'})
+
+	currentBeams := make([]int, 0, 2000)
+	// Find initial beam (S)
+	for i := 0; i < len(lines[0]); i++ {
+		if lines[0][i] == 'S' {
+			currentBeams = append(currentBeams, i)
+			break
+		}
+	}
+
+	// Start working the field!
+	for i := 1; i < len(lines); i++ {
+		tmp := make([]int, 0, 1000)
+		for _, col := range currentBeams {
+			if lines[i][col] == '^' {
+				tmp = append(tmp, col-1, col+1)
+			} else {
+				tmp = append(tmp, col)
+			}
+		}
+		currentBeams = tmp
+	}
+
+	// Get sum "current beams"
+	// sum := 0
+	// for _, v := range beamsMap {
+	// 	if v > 0 {
+	// 		sum += v
+	// 	}
+	// }
+	return len(currentBeams)
+}
+
+func part2gpt(input []byte) int {
+	lines := bytes.Split(input, []byte{'\n'})
+
+	// Find initial beam (S)
+	startCol := 0
+	for i := 0; i < len(lines[0]); i++ {
+		if lines[0][i] == 'S' {
+			startCol = i
+			break
+		}
+	}
+
+	// Count all paths using BFS/queue approach
+	type Beam struct {
+		row int
+		col int
+	}
+
+	queue := []Beam{{row: 0, col: startCol}}
+	pathCount := 0
+
+	for len(queue) > 0 {
+		beam := queue[0]
+		queue = queue[1:]
+
+		// Move beam down until it hits bottom or a splitter
+		for beam.row < len(lines)-1 {
+			beam.row++
+
+			// Check if we hit a splitter
+			if lines[beam.row][beam.col] == '^' {
+				// Split into left and right
+				leftBeam := Beam{row: beam.row, col: beam.col - 1}
+				rightBeam := Beam{row: beam.row, col: beam.col + 1}
+				queue = append(queue, leftBeam, rightBeam)
+				break // This beam path ends here (it split)
+			}
+		}
+
+		// If we reached the bottom without splitting, count this path
+		if beam.row == len(lines)-1 {
+			pathCount++
+		}
+	}
+
+	return pathCount
+}
